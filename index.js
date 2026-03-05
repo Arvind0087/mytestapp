54df3be58729c2a49942d4d07ccdc1159f29296b
Arvind Kumar <arvind.bbau2016@gmail.com>
2026-03-06 00:54:31 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "CI/CD Node App Running 🚀"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
